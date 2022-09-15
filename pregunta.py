6fa570dae7be8433f50c955e1762d499ca2391ea
paquijanoc <111616862+paquijanoc@users.noreply.github.com>
2022-09-15 13:17:56 -0500

    P1

--- pregunta.py
+++ pregunta.py
@@ -17,5 +17,14 @@ def ingest_data():
     #
     # Inserte su código aquí
     #
+    cols= ['cluster','cantidad_de_palabras_clave','porcentaje_de_palabras_clave']
+    cluster1=pd.read_fwf(
+        "clusters_report.txt",
+        colspecs="infer",
+        widths=[9, 16, 16],
+        names=cols,
+    )
+
+    df=cluster1[3:]
 
     return df
