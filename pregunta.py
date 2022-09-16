ae86cd20449494b9dc3225f6ea90dfe40098d71b
paquijanoc <111616862+paquijanoc@users.noreply.github.com>
2022-09-16 09:29:35 -0500

    P3

--- pregunta.py
+++ pregunta.py
@@ -17,14 +17,71 @@ def ingest_data():
     #
     # Inserte su código aquí
     #
-    cols= ['cluster','cantidad_de_palabras_clave','porcentaje_de_palabras_clave','principales_palabras_clave']
+    
     cluster1=pd.read_fwf(
-        'clusters_report.txt',
-        colspecs="infer",
-        widths=[9, 16, 16],
-        names=cols,
+        "https://raw.githubusercontent.com/ciencia-de-los-datos/data-ingestion---clusters-report-paquijanoc/main/clusters_report.txt",
+        widths=[9, 16, 16,84],
     )
 
-    df=cluster1[3:]
+    #Se eligen los datos unicamente y se ponen los titulos
+    df=cluster1[2:]
+    df.columns=['cluster','cantidad_de_palabras_clave','porcentaje_de_palabras_clave','principales_palabras_clave']
+
+    #Se reemplazan los nan con los valores necesarios para cluster
+    clusterlist=list(df.cluster)
+
+    lista=[]
+    for x in clusterlist:
+      y=x
+      if str(y) != 'nan':
+        lista.append(int(x))
+        z=y
+      else:
+          lista.append(int(z))
+
+    df=df.assign(cluster=lista)
+
+
+    #Se reemplazan los nan con los valores necesarios para cantidad_de_palabras_clave
+    cantidad_de_palabras_clavelist=list(df.cantidad_de_palabras_clave)
+
+    lista=[]
+    for x in cantidad_de_palabras_clavelist:
+      y=x
+      if str(y) != 'nan':
+        lista.append(int(x))
+        z=y
+      else:
+          lista.append(int(z))
+
+    df=df.assign(cantidad_de_palabras_clave=lista)
+
+    #Se reemplazan los nan con los valores necesarios para porcentaje_de_palabras_clave
+    porcentaje_de_palabras_clavelist=list(df.porcentaje_de_palabras_clave)
+
+    lista=[]
+    for x in porcentaje_de_palabras_clavelist:
+      y=x
+      if str(y) != 'nan':
+        lista.append(x)
+        z=y
+      else:
+          lista.append(z)
+
+    df=df.assign(porcentaje_de_palabras_clave=lista)
+
+    df = df.groupby(['cluster','cantidad_de_palabras_clave','porcentaje_de_palabras_clave'])['principales_palabras_clave'].apply(','.join).reset_index()
+    df.porcentaje_de_palabras_clave = df.porcentaje_de_palabras_clave.str.strip('%')
+    df['porcentaje_de_palabras_clave'] = df['porcentaje_de_palabras_clave'].str.replace(",", ".").astype(float)
+
+    df['principales_palabras_clave'] = df['principales_palabras_clave'].str.replace(".", "", regex=True)
+    df['principales_palabras_clave'] = df['principales_palabras_clave'].str.replace("  ","/",regex=True)
+    df['principales_palabras_clave'] = df['principales_palabras_clave'].str.replace("/"," ",regex=True)
+    df['principales_palabras_clave'] = df['principales_palabras_clave'].str.replace("   ","/",regex=True)
+    df['principales_palabras_clave'] = df['principales_palabras_clave'].str.replace("/"," ",regex=True)
+    df['principales_palabras_clave'] = df['principales_palabras_clave'].str.replace("    ","/",regex=True)
+    df['principales_palabras_clave'] = df['principales_palabras_clave'].str.replace("/"," ",regex=True)
+    df['principales_palabras_clave'] = df['principales_palabras_clave'].str.replace("  ","/",regex=True)
+    df['principales_palabras_clave'] = df['principales_palabras_clave'].str.replace("/"," ",regex=True)
 
     return df
